5e04b79946baf8422a5ea72177d61b04f5160ef3
azarnuzy <azarnuzy@gmail.com>
2022-08-10 20:40:18 +0700

    add img, make navbar

---
+++ main.js
@@ -0,0 +1,7 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+  document
+    .querySelector('nav')
+    .classList.toggle('window-scroll', window.scrollY > 0);
+});
