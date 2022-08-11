1e3d5cd257d251627f0d4bbbc114610d6935b548
azarnuzy <azarnuzy@gmail.com>
2022-08-12 00:41:44 +0700

    make landing page responsive

--- main.js
+++ main.js
@@ -5,3 +5,44 @@ window.addEventListener('scroll', () => {
     .querySelector('nav')
     .classList.toggle('window-scroll', window.scrollY > 0);
 });
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    // change icon
+    const icon = faq.querySelector('.faq__icon i');
+    if (icon.className === 'uil uil-angle-down') {
+      icon.className = 'uil uil-angle-up';
+    } else {
+      icon.className = 'uil uil-angle-down';
+    }
+  });
+});
+
+// show/hide nav menu
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+const body = document.querySelector('body');
+
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'flex';
+  closeBtn.style.display = 'inline-block';
+  menuBtn.style.display = 'none';
+  body.style.background = 'rgba(0, 0, 0, 0.6)';
+});
+
+// close nav menu
+const closeNav = () => {
+  menu.style.display = 'none';
+  closeBtn.style.display = 'none';
+  menuBtn.style.display = 'inline-block';
+  body.style.background = 'rgba(0, 0, 0, 0)';
+};
+
+closeBtn.addEventListener('click', closeNav);
